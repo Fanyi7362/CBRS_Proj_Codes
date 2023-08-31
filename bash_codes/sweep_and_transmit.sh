f8cd09eed6dc301a7e25140010ca9e7a7a41ffb1
Fanyi7362 <fanyi.sjtu.ee@gmail.com>
2023-08-30 23:40:40 -0400

    bash

--- bash_codes/sweep_and_transmit.sh
+++ bash_codes/sweep_and_transmit.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+sleep 5
 # Loop through frequencies
-for freq in $(seq 3550e6 5e6 3600e6); do
+for freq in $(seq 3550e6 5e6 3700e6); do
   # Start the RX measurement on PC2 using SSH
-  ssh pc6228@192.168.0.2 "timeout --signal=INT 10s ~/fanyi/LTEScope_CSI/build/lib/examples/cell_measurement -f $freq -n 500" &
+  ssh pc6228@192.168.0.2 "timeout --signal=INT 15s ~/fanyi/LTEScope_CSI/build/lib/examples/cell_measurement -f $freq -n 500" &
   # Let's assume it takes a very short time for PC2's script to initiate
   
   # Start the TX on PC1
-  ~/fanyi/NG-Scope/build/lib/examples/pdsch_enodeb -f $freq -n 500 -p 25 
+  ~/fanyi/NG-Scope/build/lib/examples/pdsch_enodeb -f $freq -n 500 -p 25 -a "addr=192.168.10.5"
   
   # Wait for both processes to complete (given they run for approximately the same time)
   wait
